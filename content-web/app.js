b147f675919ecc9df59e5d017c13a0b294fd228e
Lisa-Sun <c.lisa.sun@gmail.com>
2022-03-15 20:43:17 +0000

    Update app.js

--- content-web/app.js
+++ content-web/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const request = require('requecst');
+const request = require('request');
 
 const app = express();
 const appInsights = require("applicationinsights");
